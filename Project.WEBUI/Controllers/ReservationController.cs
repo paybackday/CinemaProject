e58a5cc84febfe7d1fef8c2269fdabbb40270be2
Emre GÖREN <emregoren98@yandex.com>
2021-02-01 00:26:29 +0300

    ReservationController Eklendi

---
+++ Project.WEBUI/Controllers/ReservationController.cs
@@ -0,0 +1,28 @@
+﻿using Project.BLL.DesignPatterns.GenericRepository.ConcRep;
+using Project.WEBUI.Models.VMClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project.WEBUI.Controllers
+{
+    public class ReservationController : Controller
+    {
+        SeatRepository _sRep;
+        public ReservationController()
+        {
+            _sRep = new SeatRepository();
+        }
+        // GET: Reservation
+        public ActionResult Seat(int MovieID,int SaloonID,int SessionID)
+        {
+            SeatVM svm = new SeatVM
+            {
+                Seats = _sRep.Where(x => x.SeatActive == false && x.SaloonID==SaloonID)
+            };
+            return View(svm);
+        }
+    }
+}
---
+++ Project.WEBUI/Models/VMClasses/SeatVM.cs
@@ -0,0 +1,13 @@
+﻿using Project.ENTITIES.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project.WEBUI.Models.VMClasses
+{
+    public class SeatVM
+    {
+        public List<Seat> Seats { get; set; }
+    }
+}
