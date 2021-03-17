58741fc9a9ea0add4a11fab768e990e2b8df85f9
Emre GÖREN <emregoren98@yandex.com>
2021-03-18 00:31:23 +0300

    PanelLayout AND PanelTools

---
+++ Project.WEBUI/Areas/Panel/Controllers/MainController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project.WEBUI.Areas.Panel.Controllers
+{
+    public class MainController : Controller
+    {
+        // GET: Panel/Main
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
